a5adc63cbeadb49e57495a5d0032ea345bf1cde2
Die-Gutierrez <dgutierrezm@unjbg.edu.pe>
2022-10-01 15:37:10 -0500

    Creacion de las funciones insertarCola y colavacia, funcionando corractamente

---
+++ colas.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Nodo
+{
+    int dato;
+    Nodo *siguiente;
+};
+
+void insertarCola(Nodo *&frente, Nodo *&fin, int n);
+bool cola_vacia(Nodo *);
+
+int main()
+{
+    Nodo *frente = NULL;
+    Nodo *fin = NULL;
+
+
+    return 0;
+}
+//Funcion para insertar elementos a una cola
+void insertarCola(Nodo *&frente, Nodo *&fin, int n)
+{
+    Nodo *nuevo_nodo = new Nodo ();
+
+    nuevo_nodo->dato = n;
+    nuevo_nodo->siguiente = NULL;
+    if(cola_vacia(frente))
+    {
+        frente = nuevo_nodo;
+    }
+    else
+    {
+        fin->siguiente = nuevo_nodo;
+    }
+    fin = nuevo_nodo;
+    cout<<"Elemento "<<n<<" insertado correctamente . . ."<<endl;
+}
+// Funcion para determinar si la cola esta vacia o no.
+bool cola_vacia(Nodo *frente)
+{
+    if(frente == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
